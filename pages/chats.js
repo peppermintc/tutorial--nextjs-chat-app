f2383270a1ed3482ed3acca8f3cc743257d84999
peppermintc <haminjp@naver.com>
2021-12-15 12:55:14 +0900

    add chat engine component

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ChatEngine } from "react-chat-engine";
+import { Context } from "../context";
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+  const [showChat, setShowChat] = useState(false);
+  const { username, secret } = useContext(Context);
+
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true);
+    }
+  });
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 200px)"
+          projectID="f31190c1-18d9-4039-aafe-ce15718d80bc"
+          userName={username}
+          userSecret={secret}
+        />
+      </div>
+    </div>
+  );
 }
