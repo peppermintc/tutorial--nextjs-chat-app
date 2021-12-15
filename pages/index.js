9a34923430abc2627500829f7f83a53d29095e9c
peppermintc <haminjp@naver.com>
2021-12-15 11:38:56 +0900

    create auth form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
+          <div className="auth-title">NextJS Chat</div>
+
+          <div className="input-container">
+            <input placeholder="Email" className="text-input" />
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Login / Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
