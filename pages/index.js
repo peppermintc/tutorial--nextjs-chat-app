eb5c64e271a4447a9d62307088c2aa47f2dd297d
peppermintc <haminjp@naver.com>
2021-12-15 12:24:15 +0900

    add onSubmit function

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
 import React, { useContext } from "react";
 import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Auth() {
-  const { setUsername, setSecret } = useContext(Context);
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if (username.length === 0 || secret.length === 0) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        { username, secret },
+        { headers: { "PRIVATE-KEY": "90a40b93-c022-452b-beed-6a37368f8675" } }
+      )
+      .then((res) => router.push("/chats"));
+  }
 
   return (
     <div className="background">
       <div className="auth-container">
-        <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
           <div className="auth-title">NextJS Chat</div>
 
           <div className="input-container">
